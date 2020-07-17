88d944be35acf332e2c1de3811391cd50da4d9af
Nailfighter <nailfighter000@gmail.com>
2020-07-17 11:38:05 +0530

    A lot of stuff

---
+++ Assets/Scripts/Music_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music_Controller : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Invoke("Load_After_Splash",3f);
+    }
+
+    private void Load_After_Splash()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player_Controller : MonoBehaviour
+{
+    void Update()
+    {
+        float Horizontal_Throw = CrossPlatformInputManager.GetAxis("Horizontal");
+        print(Horizontal_Throw);
+    }
+}
