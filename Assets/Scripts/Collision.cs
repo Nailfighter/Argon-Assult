ce4e706b5c0546a7a47075aada2861b5d66c4d9b
Nailfighter <nailfighter000@gmail.com>
2020-08-02 00:30:38 +0530

    COmplete animation done

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Collision_Trigger : MonoBehaviour
+public class Collision : MonoBehaviour
 {
-    bool trigger_suc=true;
+    public bool trigger_suc = true;
     public GameObject explosion;
     [SerializeField] float wait_time;
     public void OnTriggerEnter(Collider Other)
     {
-        GetComponent<Player_Controller>().enabled = false;
-        explosion.SetActive(true);
-        Invoke("Player_Death_Seq", wait_time);  
+        if(trigger_suc)
+        {
+            GetComponent<Player_Controller>().enabled = false;
+            explosion.SetActive(true);
+            Invoke("Player_Death_Seq", wait_time);
+        }
     }
     private void Player_Death_Seq()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
+
